d8bee4bfb685fe3e13d40c3bccf7cfb153e6c9e5
MamaSophie <belhafiinsaf@gmail.com>
2024-10-02 10:50:05 +0200

    Add files via upload
    add page creerFormation

---
+++ CreerFormation.aspx
@@ -0,0 +1,88 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="CreerFormation.aspx.cs" Inherits="CreerFormation" %>
+<%@ Register TagPrefix="AjaxToolkit" Namespace="AjaxControlToolkit" Assembly="AjaxControlToolkit"%>
+<%@ Register Src="~/UserControls/UC_Header.ascx" TagPrefix="uc" TagName="UC_Header" %>
+<%@ Register Src="~/UserControls/UC_ErrorPanel.ascx" TagPrefix="uc" TagName="UC_ErrorPanel" %>
+<%@ Register Src="~/UserControls/UC_Footer.ascx" TagPrefix="uc" TagName="UC_Footer" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Formation</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <asp:ScriptManager ID="ScriptManager1" runat="server"  EnableScriptGlobalization="true" EnableScriptLocalization="true"></asp:ScriptManager>       
+    <uc:UC_Header ID="UC_Header1" runat="server" />    
+    <div id="main" class="main" style="width:75%;" >
+        <asp:UpdatePanel ID="UpdatePanel4" runat="server" UpdateMode="Always" >                                                  
+        <ContentTemplate>      
+        <asp:Image ID="Image1" runat="server"  ImageUrl="Styles/Images/formations.png" Width="30" />
+        <asp:Label ID="Label1" runat="server" Text="Créer une formation"  CssClass="PageTitle"   ></asp:Label>
+        <table style="margin-top:20px;">
+            <tr>                
+                <td style="width:100%;vertical-align:top">    
+                    <div style="text-align:left;vertical-align:top;">
+                        <asp:Label ID="Label6" runat="server" Text="Nom:" CssClass="LibelleTB" style="display:inline;"    ></asp:Label>                         
+                        <asp:TextBox ID="TBFormation" AutoPostBack="false" runat="server"  placeholder="Titre de la formation" CssClass="form-control" Width="400" style="margin-left:10px;display:inline;" ></asp:TextBox>                                                                                                                   
+                    </div>   
+                    <div style="text-align:left;vertical-align:top;margin-top:20px;">                    
+                    <asp:UpdatePanel ID="UpdatePanel1" Visible="true" runat="server" UpdateMode="Always" >                                                  
+                    <ContentTemplate>      
+                    <div>                    
+                    <asp:Label ID="Label2" runat="server" Text="Fichier:" CssClass="LibelleTB" style="display:inline;"></asp:Label>
+                    <asp:Label ID="LabFile" runat="server" Text="" CssClass="LibelleTB" style="margin-left:10px;margin-right:10px;width:100px;display:inline;"></asp:Label>
+                    <div class="fileUpload btn btn-dore hvr-push" style="margin-left:-3px;">                    
+                    <span>Choisir</span>
+                    <asp:FileUpload ID="FileUpload1" runat="server" AllowMultiple="false"  CssClass="upload" />                    
+                    </div>                   
+                   <asp:ImageButton ID="BtnUpload" runat="server"  ImageUrl="Styles/Images/BtnUpload.png"  ImageAlign="AbsMiddle"  />
+                   <asp:Label ID="LabUpload" runat="server" Text="Envoyer" ></asp:Label>                   
+                   </div>                                      
+                   </ContentTemplate>
+                         <Triggers>                             
+                             <asp:PostBackTrigger ControlID="BtnUpload" />
+                         </Triggers>
+                   </asp:UpdatePanel>
+                    </div>
+                     <div style="text-align:left;vertical-align:top;margin-top:20px;">
+                        <asp:Label ID="Label3" runat="server" Text="Thème :" CssClass="LibelleTB" style="display:inline;"    ></asp:Label>                                                 
+                         <asp:DropDownList ID="DDLThemes" CssClass="form-control" Width="400" style="float:right;margin-right:190px;display:inline;"  runat="server"></asp:DropDownList>
+                    </div>   
+                    <div style="text-align:left;vertical-align:top;margin-top:40px;">
+                        <asp:Label ID="Label4" runat="server" Text="Contact :" CssClass="LibelleTB" style="display:inline;"    ></asp:Label>                                                 
+                         <asp:DropDownList ID="DDLContacts" CssClass="form-control" Width="400" style="float:right;margin-right:190px;display:inline;"  runat="server"></asp:DropDownList>
+                    </div>   
+                    <div style="text-align:left;vertical-align:top;margin-top:40px;">
+                        <asp:Label ID="Label5" runat="server" Text="Durée :" CssClass="LibelleTB" style="display:inline;"    ></asp:Label>                                                 
+                        <asp:TextBox ID="TBDuree" AutoPostBack="false" runat="server"  placeholder="" CssClass="form-control" Width="70" style="margin-left:5px;display:inline;" ></asp:TextBox>                                                                                                                   
+                        <asp:DropDownList ID="DDLDuree" CssClass="form-control" Width="70" style="margin-left:8px;display:inline;" runat="server">
+                            <asp:ListItem Value="J" Text="J" Selected="True"></asp:ListItem>
+                            <asp:ListItem Value="H" Text="H"></asp:ListItem>
+                        </asp:DropDownList>
+                    </div>   
+                    <div style="text-align:left;vertical-align:top;margin-top:30px;">
+                        <asp:Label ID="Label8" runat="server" Text="Accès :" CssClass="LibelleTB" style="display:inline;"    ></asp:Label>                                                 
+                         <asp:DropDownList ID="DDLAcces" CssClass="form-control" Width="400" style="float:right;margin-right:190px;display:inline;"  runat="server"></asp:DropDownList>
+                    </div>   
+                    <div style="text-align:left;vertical-align:top;margin-top:30px;">
+                        <asp:Label ID="Label7" runat="server" Text="Enseigne:" CssClass="LibelleTB" style="display:inline-block;margin-right:0px;"    ></asp:Label>
+                        <asp:CheckBoxList ID="CBEnseigne" SelectMethod="" runat="server" TextAlign="Right" CssClass="rbl"  style="display:inline-block;margin-bottom:-10px;margin-left:20px;"   RepeatDirection="Horizontal"  DataTextField="Libelle_Enseigne" DataValueField="Id_Typ_enseigne"></asp:CheckBoxList>                        
+                    </div>
+                    <div style="text-align:left;vertical-align:top;margin-top:30px;">                        
+                        <asp:CheckBox ID="CBEmail" runat="server" Checked="true" style="display:inline-block;margin-top:0px;margin-left:0px;" />                        
+                        <asp:Label ID="Label9" runat="server" Text="Envoyer des emails" CssClass="LibelleTB" style="display:inline-block;height:30px;margin-right:0px;margin-bottom:10px;"></asp:Label>
+                    </div>                    
+                </td>   
+            </tr>
+        </table>                        
+            <div style="width:auto;float:right;vertical-align:top;margin-top:0px;margin-right:0px;">
+                        <asp:Button ID="BtnAdd" runat="server" Text="Créer" CssClass="btn btn-dore hvr-push"  UseSubmitBehavior="false"  style="width:200px;margin-top:0px;margin-left:0px;display:inline;"/>
+                        <asp:Button ID="BtnCancel" runat="server" Text="Annuler" CssClass="btn btn-default hvr-push" UseSubmitBehavior="false"  style="margin-top:0px;margin-left:50px;margin-right:20px;display:inline;"/>
+            </div>                    
+          </ContentTemplate>
+        </asp:UpdatePanel>
+        <uc:UC_ErrorPanel ID="UC_ErrorPanel1" runat="server" />
+    </div>
+    <uc:UC_Footer  runat="server" ID="UC_Footer1"/>
+    </form>
+</body>
+</html>
---
+++ CreerFormation.aspx.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DAL;
+using System.IO;
+
+public partial class CreerFormation : System.Web.UI.Page
+{
+    private int typeContact = 1;
+    private string uploadPdfFolder = "Upload/Pdf/Formations/";
+    ILogger _log = LogHelper.Logger;
+    private bool isCreateMode
+    {
+        get { return (bool)ViewState["isCreateMode"]; }
+        set { ViewState["isCreateMode"] = value; }
+    }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // CONTROLE HABILITATION //
+        if (!UC_Header1.IsAdminRole)
+        {
+            _log.Info("Utilisateur n'appartient pas au groupe B-APPL-INSCRIPTION_FORMATION, redirection vers default.aspx");
+            Response.Redirect("default.aspx");
+        }
+        // CONTROLE HABILITATION //END
+        BtnAdd.Text = "Créer";
+        BtnAdd.Click+=BtnAdd_Click;
+        BtnCancel.Click+=BtnCancel_Click;
+        BtnUpload.Click += BtnUpload_Click;
+        if (!Page.IsPostBack)
+        {
+            isCreateMode = true;
+            var tmpModel = new ModelManager();                       
+            DDLThemes.DataSource = (from t in tmpModel.GetThemes().FindAll(t => t.Statut_tm) orderby t.Lib_Theme select t);
+            DDLThemes.DataTextField = "lib_theme";
+            DDLThemes.DataValueField = "id_theme";
+            DDLThemes.DataBind();
+            DDLThemes.Items.Insert(0, new ListItem("Sélectionner un thème", "", true));
+            DDLThemes.SelectedIndex = 0;
+
+            DDLContacts.DataSource = (from u in tmpModel.GetUsersCreerFormation(typeContact) orderby u.Nom_U select new { nomcomplet = u.Prenom_U + " " + u.Nom_U, id = u.Id_U });
+            DDLContacts.DataTextField = "nomcomplet";
+            DDLContacts.DataValueField = "id";
+            DDLContacts.DataBind();
+            DDLContacts.Items.Insert(0, new ListItem("Sélectionner un contact", "", true));
+            DDLContacts.SelectedIndex = 0;
+
+            DDLAcces.DataSource = tmpModel.Acces_Type.ToList();
+            DDLAcces.DataValueField = "id_acces";
+            DDLAcces.DataTextField = "lib_acces";
+            DDLAcces.DataBind();
+            DDLAcces.Items.Insert(0, new ListItem("Sélectionner un accès", "", true));
+            DDLAcces.SelectedIndex = 0;
+
+            CBEnseigne.DataBound += CBEnseigne_DataBound;
+            CBEnseigne.DataSource = tmpModel.GetEnseignes();
+            CBEnseigne.DataTextField = "Libelle_Enseigne";
+            CBEnseigne.DataValueField = "Id_Typ_enseigne";
+            CBEnseigne.DataBind();
+
+            if (Request["idFormation"] != null)
+            {
+                _log.Info(string.Format("Modifier Formation, IdFormation = {0}", Convert.ToInt32(Request["idFormation"])));
+                var tmpFormation = tmpModel.GetFormations(Convert.ToInt32(Request["idFormation"])).FirstOrDefault();
+                if (tmpFormation != null) bindDataToControls(tmpFormation);
+                isCreateMode = false;                
+            }
+        }
+        BtnAdd.Text = !isCreateMode?"Modifier":"Créer";
+        Label1.Text = !isCreateMode ? "Modifier une formation":"Créer une formation";
+    }
+    private void CBEnseigne_DataBound(object sender, EventArgs e)
+    {
+        foreach (ListItem item in CBEnseigne.Items)
+        {
+          item.Attributes["style"] += "margin-right:10px";
+        }
+    }
+    void BtnUpload_Click(object sender, ImageClickEventArgs e)
+    {
+        try
+        {
+            var tmpFileUpload = FileUpload1;
+            string tmpFileFullName = "";
+            if (tmpFileUpload.HasFile && tmpFileUpload.FileBytes.Length > 0 && (tmpFileUpload.FileName.EndsWith(".pdf")))
+            {
+                tmpFileFullName += tmpFileUpload.FileName;
+                tmpFileUpload.SaveAs(MapPath(uploadPdfFolder) + tmpFileFullName);
+                if (!File.Exists(MapPath(uploadPdfFolder) + tmpFileFullName))
+                {
+                    UC_ErrorPanel1.DisplayError("Fichier non téléchargé. Vérifiez les droits en écriture sur le serveur.");
+                    return;
+                }
+            }
+            else
+            {
+                UC_ErrorPanel1.DisplayError("Fichier non conforme.");
+                return;
+            }
+            LabFile.Text = tmpFileFullName;
+        }
+        catch(Exception ex)
+        {
+            _log.Error("Erreur dans CreerFormation, BtnUpload_Click : ", ex); 
+        }        
+    }
+    void BtnCancel_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("ListeFormations.aspx");
+    }
+    void BtnAdd_Click(object sender, EventArgs e)
+    {
+        _log.Info("CreerFormation, Début Ajout Formation");
+        try
+        {
+            var tmpCheckErrors = checkData();
+            if (!string.IsNullOrEmpty(tmpCheckErrors))
+            {
+                UC_ErrorPanel1.DisplayError(tmpCheckErrors);
+                return;
+            }
+            var tmpModel = new ModelManager();
+            Formation tmpFormation = null;
+            if (Request["idFormation"] != null)
+            {
+                _log.Info(string.Format("Modifier Formation, IdFormation = {0}", Convert.ToInt32(Request["idFormation"])));
+                tmpFormation = tmpModel.GetFormations(Convert.ToInt32(Request["idFormation"])).FirstOrDefault();
+                tmpFormation.Date_Update = DateTime.Now;
+            }
+            else
+            {
+                _log.Info("Request['idFormation'] = null");
+                tmpFormation = new Formation();
+                tmpFormation.Date_Creation = DateTime.Now;
+            }
+            tmpFormation.Duration = Convert.ToDecimal(TBDuree.Text);
+            tmpFormation.Type_Duration = DDLDuree.SelectedValue;
+            tmpFormation.eMail_Auto = CBEmail.Checked;
+            tmpFormation.fk_Id_Acces = Convert.ToInt32(DDLAcces.SelectedValue);
+            tmpFormation.fk_Id_Theme = Convert.ToInt32(DDLThemes.SelectedValue);
+            tmpFormation.fk_id_u_Contact = Convert.ToInt32(DDLContacts.SelectedValue);
+            tmpFormation.Lib_fm = TBFormation.Text;
+            tmpFormation.Statut_fm = tmpFormation.Id_fm > 0 ? tmpFormation.Statut_fm : true;
+            tmpFormation.File_fm = LabFile.Text;
+            tmpFormation.Formation_Enseigne.Clear();
+            foreach (ListItem cb in CBEnseigne.Items)
+            {
+                if (cb.Selected) tmpFormation.Formation_Enseigne.Add(new Formation_Enseigne { FK_ID_Typ_Enseigne = Convert.ToInt32(cb.Value), FK_Id_Form = tmpFormation.Id_fm });
+            }
+            var retour = tmpModel.SetFormation(tmpFormation);
+            if (!string.IsNullOrEmpty(retour))
+            {
+                UC_ErrorPanel1.DisplayError(retour);
+            }
+            else
+            {
+                Response.Redirect("ListeFormations.aspx",false);
+            }
+        }
+        catch(Exception ex)
+        { 
+            _log.Error("Erreur dans CreerFormation, BtnAdd_Click :", ex);
+        }
+        _log.Info("CreerFormation, Fin Ajout Formation");
+    }
+    string checkData()
+    {
+        string retour = "";
+        if(string.IsNullOrEmpty(TBFormation.Text)) retour += "Veuillez saisir un nom de formation<br>";
+        if (DDLThemes.SelectedIndex==0) retour += "Veuillez choisir un thème<br>";
+        if (DDLContacts.SelectedIndex == 0) retour += "Veuillez choisir un contact<br>";
+        if (string.IsNullOrEmpty(TBDuree.Text) || !ToolBox.IsNumeric(TBDuree.Text) || double.Parse(TBDuree.Text)<=0 ) retour += "Veuillez saisir une durée > 0<br>";        
+        if (DDLAcces.SelectedIndex == 0) retour += "Veuillez choisir un accès<br>";
+        if (CBEnseigne.SelectedIndex < 0) retour += "Veuillez sélectionner une enseigne <br>";        
+        return retour;
+    }
+    void bindDataToControls(Formation pFormation)
+    {
+        try
+        {
+            TBFormation.Text = pFormation.Lib_fm;
+            LabFile.Text = pFormation.File_fm;
+            DDLThemes.SelectedValue = pFormation.fk_Id_Theme.ToString();
+            DDLContacts.SelectedValue = pFormation.fk_id_u_Contact.ToString();
+            TBDuree.Text = pFormation.Duration.ToString();
+            DDLDuree.SelectedValue = pFormation.Type_Duration;
+            DDLAcces.SelectedValue = pFormation.fk_Id_Acces.ToString();
+            foreach (ListItem item in CBEnseigne.Items)
+            {
+                if (new ModelManager().Formation_Enseigne.ToList().Exists(e => e.FK_Id_Form == pFormation.Id_fm && e.FK_ID_Typ_Enseigne.ToString() == item.Value)) item.Selected = true;
+            }
+            CBEmail.Checked = pFormation.eMail_Auto;
+            TBDuree.Enabled = !pFormation.T_Sessions.Any(s => s.Statut_Ses);
+            DDLDuree.Enabled = TBDuree.Enabled;
+        }
+        catch (Exception ex)
+        {
+            _log.Error(ex.Message, ex);
+        }
+    }
+}
